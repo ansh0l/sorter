d311e4d3b3aad35a84170bcf973954e212047e71
ansh0l <anshl.goyl@gmail.com>
2014-12-08 23:22:53 +0530

    changes to data_generator logic

--- data_generator.py
+++ data_generator.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
-
 import sys
+import random
 
-import pdb; pdb.set_trace()
-#if sys.
+low, high = 0, 10**6
+output = sys.argv[1] if len(sys.argv) > 1 else "output.txt"
+
+with open(output, "w") as f:
+    elements = []
+    for i in range(low, high):
+        elements.append(random.randint(low, high))
+    if len(sys.argv) == 3 and sys.argv[2].lower() != "reps=true":
+        elements = set(elements)
+    f.write(' '.join(str(i) for i in elements))
+
