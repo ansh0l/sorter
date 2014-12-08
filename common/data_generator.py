7e0e5fc8ac3e4a8ae29f144100f8647663ca5541
ansh0l <anshl.goyl@gmail.com>
2014-12-09 00:14:09 +0530

    `module`ify data_generator, adding sample output list

---
+++ common/data_generator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Features:
+
+1. Generate a list of numbers for different sorting algorithms
+2. Save them to the user specified file if inputted, else to `output.txt`
+3. If the flag reps=True is passed, allow repetition of numbers
+4. The numbers generated belong to the range (0, 1000000)
+
+Execution possibilities:
+
+./data_generator.py
+./data_generator.py outfile.txt
+./data_generator.py outfile.txt reps=True
+python data_generator.py
+python data_generator.py outfile.txt
+python data_generator.py outfile.txt reps=True
+"""
+
+import sys
+import random
+
+if __name__ == "__main__":
+
+    # pylint: disable-msg=C0103
+    low, high = 0, 10**6
+    output = sys.argv[1] if len(sys.argv) > 1 else "output.txt"
+
+    with open(output, "w") as f:
+        elements = []
+        for i in range(low, high):
+            elements.append(random.randint(low, high))
+        if len(sys.argv) == 3 and sys.argv[2].lower() != "reps=true":
+            elements = set(elements)
+        f.write(' '.join(str(i) for i in elements))
