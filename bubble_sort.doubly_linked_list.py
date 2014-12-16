ec7578a6312c6a6bd4931d92fedc9e1afe0da94a
ansh0l <anshl.goyl@gmail.com>
2014-12-16 20:55:51 +0530

    Adding working version for #BubbleSort

--- bubble_sort.doubly_linked_list.py
+++ bubble_sort.doubly_linked_list.py
@@ -17,24 +17,39 @@ class Node(object):
         self.prev = prev
 
 def bubble_sort(start):
-    """bubble sort the values"""
+    """bubble sort the values
+    If nodes are in order     |1| -> |2| -> |3| -> |4|, 
+        and |2| > |3|,
+        then the new order is |1| -> |3| -> |2| -> |4|
+    """
     change = 0
-    original_start = start
+    current = start
     import pdb; pdb.set_trace()
-    while start.next:
-        if start.next.num < start.num:
-            if not start.prev:
-                original_start = start.next
-            start.next, start.next.prev = start.next.next, start.prev
-            change += 1
-        start = start.next
-    return change, original_start
+    try:
+        while current.next:
+            if current.next.num < current.num:
+                n1, n2, n3 = current.prev, current, current.next
+                n4 = current.next.next if current.next.next else None
+                if n1: n1.next = n3
+                else: start = n3
+                if n4: n4.prev = n2
+                n2.prev = n3
+                n2.next = n4
+                n3.prev = n1
+                n3.next = n2
+                change += 1
+            else:
+                current = current.next
+    except Exception as e:
+        import pdb; pdb.set_trace()
+        print e.message
+    return change, start
 
 # pylint: disable-msg=C0103
 if __name__ == "__main__":
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.10000.txt"
     with open(filename, "r") as f:
-        numbers = [int(i) for i in f.read().split()]
+        numbers = [int(i) for i in f.read().split()][:10]
     start = curr = prev = None
 
     for idx, num in enumerate(numbers):
@@ -46,8 +61,9 @@ if __name__ == "__main__":
             prev = curr
     change = True
     while change:
+        st = start
+        while st:
+            print st.num
+            st = st.next
         change, start = bubble_sort(start)
-    while start:
-        print start.num
-        start = start.next
 
