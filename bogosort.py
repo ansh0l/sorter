33b8bfff6056e9df0feffe453fd44b2e536dc8d8
ansh0l <anshl.goyl@gmail.com>
2014-12-16 21:21:17 +0530

    Working version of #BogoSort

--- bogosort.py
+++ bogosort.py
@@ -12,7 +12,7 @@ def is_sorted(numbers):
 
 def bogo_sort(numbers):
     while not is_sorted(numbers):
-        idx1, idx2 = random.randint(0, len(numbers) - 1)
+        idx1 = idx2 = random.randint(0, len(numbers) - 1)
         while idx1 == idx2:
             idx2 = random.randint(0, len(numbers) - 1)
         numbers[idx1], numbers[idx2] = numbers[idx2], numbers[idx1]
@@ -24,5 +24,6 @@ if __name__ == "__main__":
     with open(filename, "r") as f:
         numbers = [int(i) for i in f.read().split()][:10]
 
-    sorted_numbers = bogo_sort(numbers)
-    print numbers, sorted_numbers
+    print numbers
+    numbers = bogo_sort(numbers)
+    print numbers
