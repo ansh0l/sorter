c185e3681e586e4bebf02a6c44b632dbd6abad27
ansh0l <anshl.goyl@gmail.com>
2014-12-18 17:22:21 +0530

    Working version of #MergeSort

--- merge_sort.py
+++ merge_sort.py
@@ -7,9 +7,13 @@ Sample code to sort numbers using Merge sort
 import sys
 
 def merge(list1, list2):
+    """
+    Merge two sorted sub-lists into a single list.
+    The base case is that each of these lists have 1 elements
+    """
     result = []
     len1, len2 = len(list1), len(list2)
-    i,  j = 0, 0
+    i, j = 0, 0
     while i < len1 or j < len2:
         if i == len1:
             result += list2[j:]
@@ -27,23 +31,29 @@ def merge(list1, list2):
     return result
 
 def merge_sort(numbers):
+    """
+    Apply merge sort on a list of numbers
+    This is done by recursively applying merge to sorted sub-arrays
+    """
     mid = len(numbers)/2
+    # mid will be 0 if length was 1
     if mid:
         return merge(merge_sort(numbers[:mid]), merge_sort(numbers[mid:]))
-    else: # mid will be 0 => length was 1
+    else: 
         return numbers
 
+# pylint: disable=C0103
 if __name__ == "__main__":
 
     filename = sys.argv[1] if len(sys.argv) > 1 else "input.1000000.txt"
     with open(filename, "r") as f:
-        numbers = [int(i) for i in f.read().split()]
+        numbers = [int(num) for num in f.read().split()]
 
-    for i in numbers:
-        print i
+    for num in numbers:
+        print num
 
     numbers = merge_sort(numbers)
 
     print ""
-    for i in numbers:
-        print i
+    for num in numbers:
+        print num
