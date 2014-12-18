13fc4c5c8bd220a73eb1aa8e4297d9f58613d255
ansh0l <anshl.goyl@gmail.com>
2014-12-18 17:17:41 +0530

    Adding the merge function for #MergeSort

--- merge_sort.py
+++ merge_sort.py
@@ -6,9 +6,24 @@ Sample code to sort numbers using Merge sort
 
 import sys
 
-
 def merge(list1, list2):
     result = []
+    len1, len2 = len(list1), len(list2)
+    i,  j = 0, 0
+    while i < len1 or j < len2:
+        if i == len1:
+            result += list2[j:]
+            break
+        elif j == len2:
+            result += list1[i:]
+            break
+        else:
+            if list1[i] <= list2[j]:
+                result.append(list1[i])
+                i += 1
+            else:
+                result.append(list2[j])
+                j += 1
     return result
 
 def merge_sort(numbers):
@@ -20,7 +35,7 @@ def merge_sort(numbers):
 
 if __name__ == "__main__":
 
-    filename = sys.argv[1] if len(sys.argv) > 1 else "input.10000.txt"
+    filename = sys.argv[1] if len(sys.argv) > 1 else "input.1000000.txt"
     with open(filename, "r") as f:
         numbers = [int(i) for i in f.read().split()]
 
